ecfdeba89a03f1991162bfc9dc506954769f347a
Andrew Jong <andrewjong87@gmail.com>
2018-01-17 13:41:49 -0800

    Move code for getting live data to its own route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
+const liveData = require('./routes/live-data')
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/live-data', liveData)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
