312a6392d9dee903398106e00a1c2e3d5c34f83b
Andrew Jong <andrewjong87@gmail.com>
2018-02-02 14:14:38 -0800

    Fix integration bug

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
     this.toggle = this.toggle.bind(this);
     this.state = {
       activeTab: '1',
-      nodes:[]
+      nodes: []
     };
   }
 
@@ -54,51 +54,51 @@ class App extends Component {
 
   //handles form events
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState(Object.apply(this.state, { [event.target.name]: event.target.value }));
   }
 
 
-  // componentDidMount() {
-  //   setInterval(() => {
-  //     fetch('/live-data')
-  //       .then(res => {
-  //         if (res.ok) return res.json();
-  //         else throw new Error('Could not connect');
-  //       })
-  //       .catch(err => {
-  //         console.log(err);
-  //       })
-  //       .then(latestData => {
-  //         if (!latestData) throw new Error('No data');
-  //         latestData.map(node => {
-  //           // clone the original state
-  //           const newState = Object.assign({}, this.state);
-  //           // init the array of datapoints if necessary
-  //           if (typeof newState.nodes[node.id] === 'undefined') newState.nodes[node.id] = [];
-  //           const dataPoints = newState.nodes[node.id];
-  //           const lastPoint = dataPoints[dataPoints.length - 1];
-  //           if (!(JSON.stringify(node) === JSON.stringify(lastPoint))) {
-  //             console.log('New data detected. Updating state...')
-  //             // add the new data
-  //             newState.nodes[node.id].push(node);
-  //             // don't store in the state more than max amount of datapoints
-  //             if (newState.nodes[node.id].length > MAX_DATA) newState.nodes[node.id].shift();
-  //             return this.setState({ newState });
-  //           } else {
-  //             console.log(`No new data for node id ${node.id}`)
-  //           }
-  //         });
-  //       })
-  //       .catch(err => {
-  //         console.log(err);
-  //       });
-  //   }, INTERVAL_SECONDS * 1000);
-  // }
+  componentDidMount() {
+    setInterval(() => {
+      fetch('/live-data')
+        .then(res => {
+          if (res.ok) return res.json();
+          else throw new Error('Could not connect');
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .then(latestData => {
+          if (!latestData) throw new Error('No data');
+          latestData.map(node => {
+            // clone the original state
+            const newState = Object.assign({}, this.state);
+            // init the array of datapoints if necessary
+            if (typeof newState.nodes[node.id] === 'undefined') newState.nodes[node.id] = [];
+            const dataPoints = newState.nodes[node.id];
+            const lastPoint = dataPoints[dataPoints.length - 1];
+            if (!(JSON.stringify(node) === JSON.stringify(lastPoint))) {
+              console.log('New data detected. Updating state...')
+              // add the new data
+              newState.nodes[node.id].push(node);
+              // don't store in the state more than max amount of datapoints
+              if (newState.nodes[node.id].length > MAX_DATA) newState.nodes[node.id].shift();
+              return this.setState({ newState });
+            } else {
+              console.log(`No new data for node id ${node.id}`)
+            }
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }, INTERVAL_SECONDS * 1000);
+  }
   /**
    * Hardcode the color for each sensor
    * @param {string} label 
    */
-  chooseBorderColor(label){
+  chooseBorderColor(label) {
     if (label === "humidity") return 'rgba(80,120,230,1)';
     if (label === "temp_ambient") return 'rgba(255,99,132,1)';
     if (label === "temp_ir") return 'rgba(255,99,132,1)';
@@ -119,7 +119,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <Jumbotron>
+          <Jumbotron>
             <h1 align="center"><font size="30">{LAB_NAME} Lab</font></h1>
             <center>
               <font size="20"><b>Manager: {MANAGER_NAME}<br /></b></font>
@@ -319,7 +319,7 @@ class App extends Component {
                   <FormGroup>
                     <Label for="Lab Title">Lab Name</Label>
                     <InputGroup>
-                      <Input input type="value" name="value" onChange={this.handleChange.bind(this)} value={this.state.value}  placeholder="Lab Name" />
+                      <Input input type="value" name="value" onChange={this.handleChange.bind(this)} value={this.state.value} placeholder="Lab Name" />
                     </InputGroup>
                   </FormGroup>
 
@@ -346,55 +346,50 @@ class App extends Component {
         <div className="container">
           {
             // map over each of the nodes
-            //if (this.state.nodes){
-            //  console.log('THIS IS THE STATE! HI' + JSON.stringify(this.state.nodes))
-            //} else {
-            //  console.log('no node state')
-            //}
-            // Object.keys(this.state.nodes).map(idKey => {
-            //   const values = this.state.nodes[idKey];
-            //   const lastValue = values[values.length - 1];
-            //   const owner = lastValue.owner || '';
-            //   const sensorLabels = ["humidity", "temp_ambient", "temp_ir", "carbon_monoxide", "methane", "hydrogen", "sound", "vibration", "battery"];
-            //   const sensorData = sensorLabels.map(label => {
-            //     return {
-            //       status: "Good",
-            //       chartData: {
-            //         labels: values.map(timePoint => timePoint.time),
-            //         datasets: [{
-            //           label: label,
-            //           data: values.map(timePoint => timePoint[label]),
-            //           backgroundColor: this.chooseBorderColor(label).replace(',1)', ',.5)'),
-            //           borderColor: this.chooseBorderColor(label),
-            //           borderWidth: 1
-            //         }],
-            //       },
-            //       chartOptions: {
-            //         legend: {
-            //           display: true,
-            //           position: 'top'
-            //         },
-            //         scales: {
-            //           yAxes: [{
-            //             ticks: {
-            //               beginAtZero: true
-            //             }
-            //           }]
-            //         },
-            //       }
-            //     }
-            //   });
+            Object.keys(this.state.nodes).map(idKey => {
+              const values = this.state.nodes[idKey];
+              const lastValue = values[values.length - 1];
+              const owner = lastValue.owner || '';
+              const sensorLabels = ["humidity", "temp_ambient", "temp_ir", "carbon_monoxide", "methane", "hydrogen", "sound", "vibration", "battery"];
+              const sensorData = sensorLabels.map(label => {
+                return {
+                  status: "Good",
+                  chartData: {
+                    labels: values.map(timePoint => timePoint.time),
+                    datasets: [{
+                      label: label,
+                      data: values.map(timePoint => timePoint[label]),
+                      backgroundColor: this.chooseBorderColor(label).replace(',1)', ',.5)'),
+                      borderColor: this.chooseBorderColor(label),
+                      borderWidth: 1
+                    }],
+                  },
+                  chartOptions: {
+                    legend: {
+                      display: true,
+                      position: 'top'
+                    },
+                    scales: {
+                      yAxes: [{
+                        ticks: {
+                          beginAtZero: true
+                        }
+                      }]
+                    },
+                  }
+                }
+              });
 
-            //   return (
-            //     <NodeCard title={`Node ${idKey}`}
-            //       ownerInfo={{
-            //         name: owner,
-            //         email: `${owner.split(' ').join('.')}@nasa.gov`
-            //       }}
-            //       description={lastValue.description}
-            //       sensorData={sensorData}
-            //     />);
-            // })
+              return (
+                <NodeCard title={`Node ${idKey}`}
+                  ownerInfo={{
+                    name: owner,
+                    email: `${owner.split(' ').join('.')}@nasa.gov`
+                  }}
+                  description={lastValue.description}
+                  sensorData={sensorData}
+                />);
+            })
           }
         </div>
       </div>
