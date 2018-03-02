8707c2ed531c0d7165f9017290033dec06786e2f
Emily Le <emilyle@Emilys-MBP.ndc.nasa.gov>
2018-03-01 20:24:11 -0800

    changes to download page

--- client/src/components/Routing/DownloadPage.js
+++ client/src/components/Routing/DownloadPage.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import { Checkbox, Segment } from 'semantic-ui-react'
+import { Dropdown, Button } from 'semantic-ui-react'
+import './DownloadPage.css'
 
+//const DownloadPage = () => {
 
-const DownloadPage = () => {
-const nodeMeasurements = ['mini', 'tiny', 'small', 'large', 'big', 'huge', 'massive'];
-
+const options = [
+  { key: 'battery', text: 'Battery', value: 'battery' },
+  { key: 'carbonMonoxide', text: 'Carbon Monoxide', value: 'carbonMonoxide' },
+  { key: 'humidity', text: 'Humidity', value: 'humidity' },
+  { key: 'hydrogen', text: 'Hydrogen', value: 'hydrogen' },
+  { key: 'methane', text: 'Methane', value: 'methane' },
+  { key: 'sound', text: 'Sound', value: 'sound' },
+  { key: 'temperatureAmbient', text: 'Temperature Ambient', value: 'temperatureAmbient' },
+  { key: 'temperatureIR', text: 'Temperature IR', value: 'temperatureIR' },
+  { key: 'vibration', text: 'Vibration', value: 'Vibration' },
+]
+/*
     return (
       <div>
         {nodeMeasurements.map(size => (
-              <Checkbox key={size} label={size}/>
+              <Checkbox size="massive" key={size} label={size}/>
               
           
         ))}
       </div>
     )
 }
+*/
+/*
+const DropdownExampleMultipleSearchSelection = () => (
+  const nodeMeasurements = ['mini', 'tiny', 'small', 'large', 'big', 'huge', 'massive'];
+return (
+  <div>
+    {nodeMeasurements.map(size => (
+      <Dropdown fluid multiple search selection options={size} />
+    ))}
+  </div>
+  )
+)
+*/
 
-export default DownloadPage
+// stateOptions = [ { key: 'AL', value: 'AL', text: 'Alabama' }, ...  ]
+
+const DropdownExampleMultipleSearchSelection = () => (
+  <div className="dropMenu">
+  <Dropdown size="massive" placeholder='Select Node(s) Information to Download' 
+  fluid multiple search selection options={options} />
+    <Button primary>Primary</Button>
+  </div>
+
+  
+)
+
+export default DropdownExampleMultipleSearchSelection
