c6601ea29cb24d93f0843f8ff7467a94cd02b486
Emily Le <emilyle@Emilys-MBP.ndc.nasa.gov>
2018-03-06 21:08:46 -0800

    showing nodes to download

--- client/src/components/Routing/StartPage.js
+++ client/src/components/Routing/StartPage.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import {Form, Grid, Table, Button } from 'semantic-ui-react'
 
 const StartPage = () => (
+<div>
 
-    "hello there"
+<Grid centered>
+    <Form>
+    <Form.Field>
+<p> Set Contact Information to be displayed in website </p>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+
+<p> Input email and phone number to enter if nodes detect non-nominal behavior </p>
+    <Form.Field>
+      <label>Email Address</label>
+      <input placeholder='email' />
+    </Form.Field>
+
+
+    <Form.Field>
+      <label>Phone Number</label>
+      <input placeholder='Phone Number' />
+    </Form.Field>
+    
+    <Button type='submit'>Submit</Button>
+  </Form>
+  </Grid>
+  </div>
 )
 
 export default StartPage
--- client/src/components/Routing/ThresholdPage.js
+++ client/src/components/Routing/ThresholdPage.js
@@ -1,8 +1,113 @@
 import React, { Component } from 'react';
+import { Dropdown, Button, Segment, Checkbox, Icon, Grid, Table } from 'semantic-ui-react'
 
-const ThresholdPage = () => (
 
-    "hi again"
-)
+    
+    const TableExampleFixed = () => (
 
-export default ThresholdPage
+      <div>
+      <p>Default values set in the nodes</p>
+      <Table padded>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Nominal</Table.HeaderCell>
+            <Table.HeaderCell>Warning</Table.HeaderCell>
+            <Table.HeaderCell>Critical</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+    
+        <Table.Body>
+
+          <Table.Row>
+            <Table.Cell>Battery</Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+            <Table.Cell>
+                130-170
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell>Carbon Monoxide</Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+            <Table.Cell>
+                Jamie is a k
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell>Humidity</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell>Hydrogen</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell>Methane</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell>Sound</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.Cell>Temperature Ambient</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+
+          <Table.Row>
+            <Table.Cell>Temperature IR</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+
+          <Table.Row>
+            <Table.Cell>Vibration</Table.Cell>
+            <Table.Cell>Denied</Table.Cell>
+            <Table.Cell>
+                Jill is an alri
+            </Table.Cell>
+            <Table.Cell>Approved</Table.Cell>
+          </Table.Row>
+
+        </Table.Body>
+      </Table>
+      </div>
+    )
+    
+    export default TableExampleFixed
+
+
+//export default ThresholdPage
