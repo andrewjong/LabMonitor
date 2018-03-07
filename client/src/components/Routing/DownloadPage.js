810384112616d1677319eb638e7fb118ea54480f
Emily Le <emilyle@Emilys-MBP.ndc.nasa.gov>
2018-03-06 20:29:51 -0800

    changes to download tab

--- client/src/components/Routing/DownloadPage.js
+++ client/src/components/Routing/DownloadPage.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Dropdown, Button } from 'semantic-ui-react'
+import { Dropdown, Button, Segment, Checkbox, Icon } from 'semantic-ui-react'
 import './DownloadPage.css'
 
-//const DownloadPage = () => {
-
+/*
 const options = [
   { key: 'battery', text: 'Battery', value: 'battery' },
   { key: 'carbonMonoxide', text: 'Carbon Monoxide', value: 'carbonMonoxide' },
@@ -15,41 +14,167 @@ const options = [
   { key: 'temperatureIR', text: 'Temperature IR', value: 'temperatureIR' },
   { key: 'vibration', text: 'Vibration', value: 'Vibration' },
 ]
-/*
-    return (
-      <div>
-        {nodeMeasurements.map(size => (
-              <Checkbox size="massive" key={size} label={size}/>
-              
-          
-        ))}
-      </div>
-    )
-}
-*/
-/*
-const DropdownExampleMultipleSearchSelection = () => (
-  const nodeMeasurements = ['mini', 'tiny', 'small', 'large', 'big', 'huge', 'massive'];
-return (
-  <div>
-    {nodeMeasurements.map(size => (
-      <Dropdown fluid multiple search selection options={size} />
-    ))}
-  </div>
-  )
-)
-*/
 
-// stateOptions = [ { key: 'AL', value: 'AL', text: 'Alabama' }, ...  ]
+const renderLabel = label => ({
+  color: 'blue',
+  content: `${label.text}`,
+
+})
 
 const DropdownExampleMultipleSearchSelection = () => (
   <div className="dropMenu">
-  <Dropdown size="massive" placeholder='Select Node(s) Information to Download' 
-  fluid multiple search selection options={options} />
-    <Button primary>Primary</Button>
-  </div>
 
-  
+    <div className="searchBar">
+      <Dropdown size="massive" placeholder='Select/Search Node(s) Information to Download'
+        fluid multiple search selection options={options} renderLabel={renderLabel}/>
+    </div>
+
+    <div className="downloadButton">
+      <Button primary>Download</Button>
+    </div>
+
+  </div>
 )
 
 export default DropdownExampleMultipleSearchSelection
+
+
+
+const sensorTypes = ["humidity", "temp_ambient", "temp_ir", "carbon_monoxide", "methane", "hydrogen", "sound", "vibration", "battery"];
+
+const SegmentExampleSegment = () => (
+  <Segment>
+    Node${sensorTypes}
+  </Segment>
+)
+
+export default SegmentExampleSegment
+*/
+
+const nodeSamples = [
+  "1", "2", "3", "4"
+];
+const sensorTypes = [
+  "Battery", "Carbon Monoxide", "Humidity", "Hydrogen", "Methane",
+  "Sound", "Temperature Ambient", "Temperature IR", "Vibration"
+];
+const DownloadPage = (props) => {
+  const nodeMaps = Object.keys(props.nodes).map(idKey => {
+    const CreateSegmentMap = () => (
+      nodeSamples.map(SegmentNodes)
+    )
+
+    const SegmentNodes = nodeNumber => (
+      <Segment>
+        Node {nodeNumber}
+
+        <div className="checkMarks">
+          {CreateSenorTypesMap}
+        </div>
+
+        <div className="button">
+          <Button icon labelPosition='right' color="green">
+            Download
+        <Icon name='download' />
+          </Button>
+        </div>
+
+      </Segment>
+    )
+
+    const CreateSenorTypesMap = () => (
+      sensorTypes.map(CheckMark)
+    )
+
+    const CheckMark = checkNames => (
+      <Checkbox label={checkNames}>
+      </Checkbox>
+    )
+
+  })
+
+
+
+
+  /*
+  const DownloadPage = (props) => {
+  
+    
+    const CreateSegmentMap = () => (
+      nodeSamples.map(SegmentNodes)
+    )
+  
+    const SegmentNodes = nodeNumber => (
+      <Segment>
+        Node {nodeNumber}
+  
+        <div className="checkMarks">
+          {CreateSenorTypesMap}
+        </div>
+  
+        <div className="button">
+          <Button icon labelPosition='right' color="green">
+            Download
+          <Icon name='download' />
+          </Button>
+        </div>
+  
+      </Segment>
+    )
+  
+    const CreateSenorTypesMap = () => (
+      sensorTypes.map(CheckMark)
+    )
+  
+    const CheckMark = checkNames => (
+      <Checkbox label={checkNames}>
+      </Checkbox>
+    )
+  
+    /*
+      componentWillMount = () => {
+        this.selectedCheckboxes = new Set();
+      }
+    
+      toggleCheckbox = label => {
+        if (this.selectedCheckboxes.has(label)) {
+          this.selectedCheckboxes.delete(label);
+        } else {
+          this.selectedCheckboxes.add(label);
+        }
+      }
+    
+      handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+    
+        for (const checkbox of this.selectedCheckboxes) {
+          console.log(checkbox, 'is selected.');
+        }
+      }
+    
+      createCheckbox = label => (
+        <Checkbox
+                label={label}
+                handleCheckboxChange={this.toggleCheckbox}
+                key={label}
+            />
+      )
+    
+      createCheckboxes = () => (
+        items.map(this.createCheckbox)
+      )
+    */
+
+  return (
+    <div className="container">
+
+      {nodeMaps}
+
+    </div>
+  );
+
+
+
+}
+
+export default DownloadPage;
